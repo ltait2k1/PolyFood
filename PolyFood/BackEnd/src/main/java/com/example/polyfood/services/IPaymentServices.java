aac8fc65c35e4bfeb9adafe5cca819ad9e69fd42
ltait2k1 <letuananh2k1.works@gmail.com>
2023-08-17 15:22:27 +0700

    Tuan Anh Update

--- PolyFood/BackEnd/src/main/java/com/example/polyfood/services/IPaymentServices.java
+++ PolyFood/BackEnd/src/main/java/com/example/polyfood/services/IPaymentServices.java
@@ -1,4 +1,9 @@
 package com.example.polyfood.services;
 
+import com.example.polyfood.models.Payment;
+
+import java.util.List;
+
 public interface IPaymentServices {
+    public List<Payment> getAllPayment(int pageSize, int pageNumber);
 }
--- PolyFood/BackEnd/src/main/java/com/example/polyfood/services/ipm/PaymentServices.java
+++ PolyFood/BackEnd/src/main/java/com/example/polyfood/services/ipm/PaymentServices.java
@@ -1,8 +1,15 @@
 package com.example.polyfood.services.ipm;
 
+import com.example.polyfood.models.Payment;
 import com.example.polyfood.services.IPaymentServices;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class PaymentServices implements IPaymentServices {
+    @Override
+    public List<Payment> getAllPayment(int pageSize, int pageNumber) {
+        return null;
+    }
 }
