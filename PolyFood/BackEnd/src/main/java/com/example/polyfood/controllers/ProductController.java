d29cce4060f1ca17bb8571ae3af01328a8dd7112
ltait2k1 <letuananh2k1.works@gmail.com>
2023-08-18 09:40:05 +0700

    Tuan Anh Update

--- PolyFood/BackEnd/src/main/java/com/example/polyfood/controllers/ProductController.java
+++ PolyFood/BackEnd/src/main/java/com/example/polyfood/controllers/ProductController.java
@@ -1,20 +1,41 @@
 package com.example.polyfood.controllers;
 
 import com.example.polyfood.models.Product;
+import com.example.polyfood.models.responobj.Respon;
 import com.example.polyfood.services.ipm.ProductServices;
+import com.google.gson.Gson;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController(value = "api/version1.0")
 public class ProductController {
     @Autowired
     ProductServices productServices;
+
     @GetMapping(value = "product/getall/{pageNumber}/{pageSize}")
     public Page<Product> getAll(@PathVariable int pageNumber, @PathVariable int pageSize)
     {
         return productServices.getAllProduct(pageNumber,pageSize);
+    }
+    @PostMapping(value = "product/create")
+    public Respon<Product> createProduct(@RequestBody String product)
+    {
+        Gson gson = new Gson();
+        Product productNew = gson.fromJson(product,Product.class);
+        return productServices.creatProduct(productNew);
+    }
+    @PutMapping(value = "product/update")
+    public Respon<Product> updateProduct(@RequestBody String product)
+    {
+        Gson gson = new Gson();
+        Product productNew = gson.fromJson(product,Product.class);
+        return productServices.updatePrduct(productNew);
+    }
+    @DeleteMapping(value = "product/delete")
+    public  Respon<Product> deleteProDuct(@RequestParam int idProduct)
+    {
+        return productServices.delete(idProduct);
     }
 }
